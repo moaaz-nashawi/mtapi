182bab161516c95d2e78fabfd28c8af877669f72
Moaaz Nashawi <m.nashawi@anchormen.nl>
2023-11-06 01:41:08 +0100

    e

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can choose any available port you prefer
+
+// Define a GET endpoint
+app.get('/api/data', (req, res) => {
+    // Replace this with the data you want to send as a response
+    const responseData = {
+        message: 'This is a sample GET endpoint response',
+        data: [1, 2, 3, 4, 5],
+    };
+
+    res.json(responseData);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
